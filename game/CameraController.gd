b7c1eb8d2bdac606fcae6abcb105a2414e44f19a
Ryan Masters <alldevallday@gmail.com>
2023-09-28 22:43:35 -0400

    working on the camera
    - smoothing zoom
    - removing unused code
    - working on smoothing the camera look_at a bit

--- game/CameraController.gd
+++ game/CameraController.gd
@@ -2,7 +2,6 @@ extends Node3D
 
 @onready var camera = get_node('SpringArm3D/Camera3D')
 
-var camera_should_be
 var zoom_speed = 10
 var active_character
 var height = 20
@@ -13,11 +12,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	if camera_should_be != null \
-#		and camera.global_position != camera_should_be:
-#		camera.global_position = camera.global_position.lerp(camera_should_be, delta * zoom_speed)
 	if active_character != null:
-		global_position = active_character.global_position + Vector3(0, height, 0)
+		global_position = global_position.lerp(
+			active_character.global_position + Vector3(0, height, 0),
+			delta * zoom_speed
+		)
+		
+#		var T = global_transform.looking_at(active_character.get_look_at().global_transform.origin, Vector3(0,1,0))
+#		global_transform.basis.y = lerp(global_transform.basis.y, T.basis.y, delta*zoom_speed)
+#		global_transform.basis.x=lerp(global_transform.basis.x, T.basis.x, delta*zoom_speed)
+#		global_transform.basis.z=lerp(global_transform.basis.z, T.basis.z, delta*zoom_speed)
+		
 		camera.look_at(active_character.get_look_at().global_position)
 
 # initiates a zoom in or out
