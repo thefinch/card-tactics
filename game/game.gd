ceec12116f00c1ac1e5c1d91fe7e4d078efebd77
Ryan Masters <alldevallday@gmail.com>
2023-10-27 22:08:09 -0400

    fixing movement!
    - updating gitignore to keep out any mono log files for Godot and any Mac/Blender files
    - changing the angle that Elena can walk up things
    - creating a new manager class that is globally available
    - moving input out of the game class and into the manager class
    - removing some unused code
    - using a CSG polygon instead of 3 boxes for the bridge to make it easier to manage and fixes the edge issues (adding collision came in clutch for making movement work)
    - removing the scene that I was using to test the error
    - adding a script to eventually fill in to make the falling columns work

--- game/game.gd
+++ game/game.gd
@@ -1,14 +1,11 @@
 extends Node3D
 
-var active_character: Character
-var zoom_speed = 25
-
 @onready var camera = get_node('CameraController')
 
 # Called when the node enters the scene tree for the first time.
-func _ready():	
+func _ready():
 	# load up the map
-	var level = preload("res://levels/create_error.tscn").instantiate()
+	var level = preload("res://levels/level/level.tscn").instantiate()
 	$Map.add_child(level)
 	level.global_rotate(Vector3(0, 1, 0), -45)
 
@@ -17,46 +14,7 @@ func _ready():
 	$Team.add_child(elena)
 	var char_scale = 3
 	elena.global_scale(Vector3(char_scale, char_scale, char_scale))
-	set_active_character(elena)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-# sets the active character
-func set_active_character(new_active: Character):
-	active_character = new_active
-	camera.set_active_character(active_character)
-
-# get what we clicked on a decide what do do with it
-func handle_click():
-	var result = camera.get_what_was_clicked()
-	if result:
-		# see if we clicked somewhere we should be able to walk to
-		if result.collider.get_class() == 'StaticBody3D':
-			# set the position the agent should move to
-			active_character.set_target_position(result.position)
-		else:
-			print('clicked on', result)
-
-func _input(event):
-	# check if we're clicking on something
-	var clicked = event is InputEventMouseButton \
-		and event.button_index == MOUSE_BUTTON_LEFT \
-		and event.is_released()
-	if clicked:
-		handle_click()
-
-	# check if we're zooming in
-	var zoom_in = event is InputEventMouseButton \
-		and event.button_index == MOUSE_BUTTON_WHEEL_UP \
-		and event.is_released()
-	if zoom_in:
-		camera.zoom(-1)
-
-	# check if we're zooming out
-	var zoom_out = event is InputEventMouseButton \
-		and event.button_index == MOUSE_BUTTON_WHEEL_DOWN \
-		and event.is_released()
-	if zoom_out:
-		camera.zoom(1)
+	
+	# let the manager know what it needs to know
+	Manager.set_camera(camera)
+	Manager.set_active_character(elena)
---
+++ game/manager.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+var active_character: Character
+var camera: CameraController
+
+# tells the active character where to go
+func set_active_character_target_position(selected_position):
+	active_character.set_target_position(selected_position)
+
+# sets the active character
+func set_active_character(new_active: Character):
+	active_character = new_active
+	camera.set_active_character(active_character)
+
+# sets which camera is active
+func set_camera(new_camera:CameraController):
+	camera = new_camera
+
+func _input(event):
+	var clicked = event is InputEventMouseButton \
+		and event.button_index == MOUSE_BUTTON_LEFT \
+		and event.is_released()
+	if clicked:
+		var hit = camera.get_what_was_clicked()
+		if hit:
+			print('old way')
+			set_active_character_target_position(hit.position)
+	
+	# check if we're zooming in
+	var zoom_in = event is InputEventMouseButton \
+		and event.button_index == MOUSE_BUTTON_WHEEL_UP \
+		and event.is_released()
+	if zoom_in:
+		camera.zoom(-1)
+
+	# check if we're zooming out
+	var zoom_out = event is InputEventMouseButton \
+		and event.button_index == MOUSE_BUTTON_WHEEL_DOWN \
+		and event.is_released()
+	if zoom_out:
+		camera.zoom(1)
